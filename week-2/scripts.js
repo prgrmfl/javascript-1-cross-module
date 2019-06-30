5a4f7691c37f1103f53089ebb43b1eb6cd71dc72
prgrmfl <51049669+prgrmfl@users.noreply.github.com>
2019-06-30 09:52:36 +0300

    Add files via upload

--- week-2/scripts.js
+++ week-2/scripts.js
@@ -1,5 +1,22 @@
 // data (variables)
-
+let project = {
+    name: "Site_project",
+    link: "https://www.thinkwithgoogle.com/",
+    description: "Website page",
+}
 
 
 // functions
+function print_deets(obj) {
+    for (let key in obj) {
+        console.log(obj[key]);       
+    }
+};
+
+
+
+// onclick's
+var print_det_button = document.getElementById("print_det");
+print_det_button.onclick = function() {
+                            print_deets(project);
+                          }
